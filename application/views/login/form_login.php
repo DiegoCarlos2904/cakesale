c53cfdf33177e707cbc36395b43281f55718f4a4
DIEGO <1410758@utp.edu.pe>
2018-08-23 19:56:36 -0500

    login

--- application/views/login/form_login.php
+++ application/views/login/form_login.php
@@ -1,11 +1,11 @@
-        <?php $this->load->view('layout/header')?>
-        <div class="row">
-                        <!-- body items -->
-            <!-- load products from table -->
-             <div class="col-md-12">
+		<?php $this->load->view('layout/header')?>
+		<div class="row">
+						<!-- body items -->
+			<!-- load products from table -->
+			 <div class="col-md-12">
 
-                <div class="panel panel-default">
-                    <div class="panel-body" >
+				<div class="panel panel-default">
+					<div class="panel-body" >
 						<div class="col-md-12">
 							<?= validation_errors() ?>
 							<div class="col-md-3"><?= $this->session->flashdata('error') ?></div>
@@ -13,11 +13,11 @@
 							<?= form_open('login') ?>
 								<div class="form-group">
 									<label for="username">Nombre de usuario: </label>
-									<input type="text" class="form-control" name="username" >
+									<input type="text" class="form-control" required="" name="username" >
 								</div>
 								<div class="form-group">
 									<label for="password">Contraseña: </label>
-									<input type="password" class="form-control" name="password" >
+									<input type="password" class="form-control" required name="password" >
 								</div>
 								<div class="form-group">
 								<div class="col-md-2"></div>
@@ -27,7 +27,7 @@
 									
 									</div>
 								<div class="col-md-3">
-									<?=  anchor('register','Register',['class'=>'btn btn-default']) ?>
+									<?//=  anchor('register','Register',['class'=>'btn btn-default']) ?>
 								</div>
 								</div>
 							<?= form_close() ?>
@@ -35,14 +35,14 @@
 							<div class="col-md-3"></div>	
 						</div>  
 						
-                    </div>
+					</div>
 					
 					
-                </div>
-            </div>  
+				</div>
+			</div>  
 			
-        </div>
+		</div>
 
-        <hr>
+		<hr>
 
-        <?php $this->load->view('layout/footer')?>
+		<?php $this->load->view('layout/footer')?>
