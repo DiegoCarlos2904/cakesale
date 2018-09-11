53893b5e947a7ca8f5b0d252fe834b400629a042
DIEGO <1410758@utp.edu.pe>
2018-09-11 01:49:21 -0500

    asda
    sd

--- application/models/model_orders.php
+++ application/models/model_orders.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_orders extends CI_Model {
 	public function get_user_id_by_session() {
-		$usr_name = $this->session->userdata('username');
+		$usr_name = $this->session->userdata('usr_name');
 		$gry = $this->db->where('usr_name',$usr_name)
 				->select('usr_id')
 				->limit(1)
@@ -19,7 +19,7 @@ class Model_orders extends CI_Model {
 		$invoice = array(
 				'data'		=>	date('Y-m-d H:i:s'),
 				'due_date'	=>	date('Y-m-d H:i:s',mktime(date('H'),date('i'),date('s'),date('m'),date('d') + 1,date('Y'))),
-				'user_id'	=> $this->get_user_id_by_session(),
+				'usr_id'	=> $this->get_user_id_by_session(),
 				'total'	=>	$this->cart->total()
 		);
 		$this->db->insert('invoices',$invoice);
