85938903546bac704d2b9bbeaebd4298ddd9c2a0
DIEGO <1410758@utp.edu.pe>
2018-10-10 23:34:23 -0500

    actualiznado el mapa

--- application/views/ubicaciones.php
+++ application/views/ubicaciones.php
@@ -3,7 +3,7 @@
 			<div class="col-12">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, eos rem sunt nostrum possimus autem quia minus amet placeat laudantium, ratione aliquid odio quam voluptatibus sit, molestiae natus architecto similique?</p>
 				<div class="embed-responsive embed-responsive-16by9">
-					<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7803.528066715178!2d-77.081535!3d-12.05975!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c93f526577e3%3A0x68275cb5918ce3f1!2sAv.+Venezuela!5e0!3m2!1ses!2spe!4v1539223965730" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.656180484859!2d-77.12181345875496!3d-11.99827567362165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cdccb7f894d5%3A0x5aef6742fcc78e9e!2sCentro+Medico+CEDIMED!5e0!3m2!1ses-419!2spe!4v1539232345269" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
